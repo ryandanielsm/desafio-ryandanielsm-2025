197e79d3e47824bfc5ac433c523775a89b90a0b1
Ryan Daniel Santos Menezes <ryandanielfe@gmail.com>
2025-09-06 13:06:55 -0300

    Criando a lista de animais

--- src/abrigo-animais.js
+++ src/abrigo-animais.js
@@ -1,4 +1,15 @@
 class AbrigoAnimais {
+  constructor() {
+    this.listaAnimais = {
+      Rex: { tipo:'cão', brinquedos: ['RATO', 'BOLA'] }, 
+      Mimi: { tipo:'gato', brinquedos: ['BOLA', 'LASER'] }, 
+      Fofo: { tipo:'gato', brinquedos: ['BOLA', 'RATO', 'LASER'] }, 
+      Zero: { tipo:'gato', brinquedos: ['RATO', 'BOLA'] }, 
+      Bola: { tipo:'cão', brinquedos: ['CAIXA', 'NOVELO'] }, 
+      Bebe: { tipo:'cão', brinquedos: ['LASER', 'RATO', 'BOLA'] }, 
+      Loco: { tipo:'jabuti', brinquedos: ['SKATE', 'RATO'] } 
+    }
+  }
 
   encontraPessoas(brinquedosPessoa1, brinquedosPessoa2, ordemAnimais) {
   }
